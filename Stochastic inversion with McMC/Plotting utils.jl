c918686423fa7950d9dd5b7277d9094f7166be9f
Liu.zzzz <20036557+Zhuoliulz@users.noreply.github.com>
2026-05-28 19:16:24 -0700

    Update Plotting utils.jl

--- Stochastic inversion with McMC/Plotting utils.jl
+++ Stochastic inversion with McMC/Plotting utils.jl
@@ -5,6 +5,7 @@ using KernelDensity
 using CSV, DataFrames
 using Statistics, Distributions
 using GeoStats, GeoStatsBase
+using StatsBase
 using Printf    # Have to import this standard library. Stupid Julia pops error about not having this occasionally
 
 
